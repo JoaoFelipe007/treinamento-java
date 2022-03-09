021579d1f922c76732b6ef65a806196545a50524
JoaoFelipe007 <joaofelipedasilva687@gmail.com>
2022-03-09 15:46:10 -0300

    Orientação a Objetos - Classes Finalizada

---
+++ src/main/java/academy/devdojo/maratonaJava/javaCore/IntroducaoClasses/Dominio/CarroAtividade.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package academy.devdojo.maratonaJava.javaCore.IntroducaoClasses.Dominio;
+
+/**
+ *
+ * @author Gestão Tech
+ */
+public class CarroAtividade {
+    public String nome;
+    public String modelo;
+    public int ano;
+}
---
+++ src/main/java/academy/devdojo/maratonaJava/javaCore/IntroducaoClasses/teste/CarroTeste.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package academy.devdojo.maratonaJava.javaCore.IntroducaoClasses.teste;
+
+import academy.devdojo.maratonaJava.javaCore.IntroducaoClasses.Dominio.CarroAtividade;
+
+/**
+ *
+ * @author Gestão Tech
+ */
+public class CarroTeste {
+    public static void main(String[] args) {
+        CarroAtividade carro = new CarroAtividade();
+        CarroAtividade carro2 = new CarroAtividade();
+        
+        carro.nome = "Corolla";
+        carro.ano = 2022;
+        carro.modelo ="Toyota Corolla GLi 2.0 Flex";
+    
+        System.out.println("Meu carro é o "+carro.nome+", ele é do modelo "+carro.modelo+" do ano de "+carro.ano);
+       
+        System.out.println("///////////////////////////////////////");
+      
+        carro2.nome = "Porsche";
+        carro2.ano = 2021;
+        carro2.modelo ="sport";
+        
+        System.out.println("O meu carro é um "+carro2.nome+", do ano de "+carro2.ano+" que é um modelo "+carro2.modelo);
+    
+    }
+    
+}
