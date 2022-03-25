a5df90a691a442439648faf5c1976f6c780b726e
JoaoFelipe007 <joaofelipedasilva687@gmail.com>
2022-03-25 15:59:16 -0300

    Orientação Objeto - Aula 97

---
+++ src/main/java/academy/devdojo/maratonaJava/javaCore/Exception/Exception/ExceptionTest01.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package academy.devdojo.maratonaJava.javaCore.Exception.Exception;
+
+/**
+ *
+ * @author Gestão Tech
+ */
+import java.io.File;
+import java.io.IOException;
+
+public class ExceptionTest01 {
+
+    public static void main(String[] args) {
+        criarNovoArquivo();
+
+    }
+
+    private static void criarNovoArquivo() {
+        File file = new File("C:\\Users\\Gestão Tech\\Desktop\\Joao\\Word.docx");
+        File file2 = new File("C:\\Users\\Gestão Tech\\Desktop\\Joao\\ Excel.xls");
+        try {
+            System.out.println("arquivo2 criado " + file2.createNewFile());
+            System.out.println("arquivo criado " + file.createNewFile());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/academy/devdojo/maratonaJava/javaCore/Exception/RunTimeException/RunTimeExceptionTest01.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package academy.devdojo.maratonaJava.javaCore.Exception.RunTimeException;
+
+/**
+ *
+ * @author Gestão Tech
+ */
+public class RunTimeExceptionTest01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
