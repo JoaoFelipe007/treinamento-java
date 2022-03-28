0b316bb0654e09c3526ac020ceae2fa93406c209
JoaoFelipe007 <joaofelipedasilva687@gmail.com>
2022-03-28 09:28:30 -0300

    Orientação Objeto ,Aula 98

--- src/main/java/academy/devdojo/maratonaJava/javaCore/Exception/RunTimeException/RunTimeExceptionTest01.java
+++ src/main/java/academy/devdojo/maratonaJava/javaCore/Exception/RunTimeException/RunTimeExceptionTest01.java
@@ -14,7 +14,14 @@ public class RunTimeExceptionTest01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("O resultado da divisão é: "+divisao(8, 0));
     }
-    
+
+    private static int divisao(int A, int B) {
+        if (B == 0) {
+            throw new IllegalArgumentException("Argumento ilegal, não pode ser zero");
+        }
+        return A / B;
+
+    }
 }
