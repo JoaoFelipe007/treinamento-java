a04a070a6c42e41fe5d2c395184c310d4e5560b0
JoaoFelipe007 <joaofelipedasilva687@gmail.com>
2022-03-30 11:06:26 -0300

    Orientação Objeto - Aula 117

--- src/main/java/academy/devdojo/maratonaJava/javaCore/Date/Formatacao/LocaleTest01.java
+++ src/main/java/academy/devdojo/maratonaJava/javaCore/Date/Formatacao/LocaleTest01.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package academy.devdojo.maratonaJava.javaCore.Date.Test;
+package academy.devdojo.maratonaJava.javaCore.Date.Formatacao;
 
 import java.text.DateFormat;
 import java.util.Calendar;
---
+++ src/main/java/academy/devdojo/maratonaJava/javaCore/Date/Formatacao/NumberFormat02.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package academy.devdojo.maratonaJava.javaCore.Date.Formatacao;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
+/**
+ *
+ * @author Gestão Tech
+ */
+public class NumberFormat02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Locale localeDeafault = Locale.getDefault();
+        Locale localeUSA = Locale.US;
+        Locale localeItalia = Locale.ITALY;
+        Locale localeAle = Locale.CHINA;
+        NumberFormat[] nf = new NumberFormat[4];
+        nf[0] = NumberFormat.getCurrencyInstance(localeDeafault);// curerncy converte o numero em dinheiro e coloca no sistema monetario da pais
+        nf[1] = NumberFormat.getCurrencyInstance(localeUSA);
+        nf[2] = NumberFormat.getCurrencyInstance(localeItalia);
+        nf[3] = NumberFormat.getCurrencyInstance(localeAle);
+        double valor = 1_000.45;
+
+        for (NumberFormat numberFormat : nf) {
+            System.out.println(numberFormat.format(valor));
+        }
+
+        String valorString = "$1000.45";// Para o parse funcionar no valor vc tem que colocar a formatação correspondente a posição que escolheu
+        try {
+            System.out.println(nf[1].parse(valorString));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
